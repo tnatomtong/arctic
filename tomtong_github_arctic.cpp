0ea857ea2f904326990f4eff90323c477a9d7c26
tnatomtong <44299222+tnatomtong@users.noreply.github.com>
2020-02-03 02:10:52 +0700

    Just for fun.

---
+++ tomtong_github_arctic.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello Arctic and GitHub!\n";
+    std::cout << "I'm Tomtong." << std::endl;
+    std::cout << "This program is written just so my name will be written in C++ and kept in the Arctic.\n";
+    std::cout << "It is quite pointless, yet impressive tho." << std::endl;
+
+    std::cin.get();
+
+    return 0;
+}
